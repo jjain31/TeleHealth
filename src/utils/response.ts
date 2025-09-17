9cd4970941c6c004a9194e3ce46f9f36b38cb8e0
jjain31 <jjain312002@gmail.com>
2025-09-17 16:13:43 +0530

    custom response

---
+++ src/utils/response.ts
@@ -0,0 +1,21 @@
+import { Response } from 'express'
+
+const success = (res: Response, message = 'Success', data = null, status = 200) => {
+    return res.status(status).json({
+        success: true,
+        message,
+        data,
+        error: null,
+    })
+}
+
+const error = (res: Response, message = 'Error', error = null, status = 500) => {
+    return res.status(status).json({
+        success: false,
+        message,
+        data: null,
+        error,
+    })
+}
+
+module.exports = { success, error }
